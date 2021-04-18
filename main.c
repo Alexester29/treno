aeff1d8d39640ce4090f0f04279c5067588e138b
alessandro <alesssandrovasca1979@gmail.com>
2021-04-18 12:05:54 +0200

    prima modifica

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//prova git di gigi
+int main(int argc, char *argv[]) {
+	return 0;
+}
